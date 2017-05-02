ee3c3480122d5676b8fc1a61c9a6d6e14552a8c8
Pedro Henrique Pereira Lima <pedrolim@br.ibm.com>
2017-05-01 21:35:38 -0300

    create document array*

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,9 +1,8 @@
-angular.module('todoController', [])
-
-	// inject the Todo service factory into our controller
-	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
-		$scope.formData = {};
-		$scope.loading = true;
+angular.module('todoController', []).controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
+	$scope.formData = {};
+		$scope.spinner = false;
+		$scope.toneAnalyzed = {};
+		$scope.document = [];
 
 		// GET =====================================================================
 		// when landing on the page, get all todos and show them
@@ -17,37 +16,34 @@ angular.module('todoController', [])
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
 			if ($scope.formData.text != undefined) {
-				$scope.loading = true;
+				$scope.spinner = true;
 
 				// call the create function from our service (returns a promise object)
 				Todos.create($scope.formData)
 
 					// if successful creation, call our get function to get all the new todos
 					.success(function(data) {
-						$scope.loading = false;
 						$scope.formData = {}; // clear the form so our user is ready to enter another
 						$scope.todos = data; // assign our new list of todos
 						Todos.get()
 							.success(function(data) {
-								console.log(data);
+								$scope.spinner = false;
+								$scope.toneAnalyzed = data;
+
+								$scope.document.emotionTone = $scope.toneAnalyzed.document_tone.tone_categories[0];
+								$scope.document.languageTone = $scope.toneAnalyzed.document_tone.tone_categories[1];
+								$scope.document.socialTone = $scope.toneAnalyzed.document_tone.tone_categories[2];
+
+
+								console.log($scope.document);
 							})
 							.error(function(error) {
+								$scope.spinner = false;
 								console.log(error);
 							});
 					});
 			}
 		};
 
-		// DELETE ==================================================================
-		// delete a todo after checking it
-		$scope.deleteTodo = function(id) {
-			$scope.loading = true;
-
-			Todos.delete(id)
-				// if successful creation, call our get function to get all the new todos
-				.success(function(data) {
-					$scope.loading = false;
-					$scope.todos = data; // assign our new list of todos
-				});
-		};
-	}]);
+		
+}])
